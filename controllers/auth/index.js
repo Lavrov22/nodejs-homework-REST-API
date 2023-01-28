3b610b492d9539483a17c0770eb58312a60227a8
Lavrov Andrey <Sattarlav@gmail.com>
2023-01-28 19:54:19 +0200

    avatar

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,11 +2,13 @@ const signUp = require('./signup');
 const login = require('./login');
 const current = require('./current')
 const logout = require('./logout')
+const avatar = require('./avatar')
 
 
 module.exports = {
     signUp,
     login,
     current,
-    logout
+    logout,
+    avatar
 }
